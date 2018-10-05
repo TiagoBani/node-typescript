6dcef4da21b5021a54307f06873f05cce2554d0a
Tiago Bani <tiago.bani@yahoo.com.br>
2018-10-05 04:06:25 -0300

    adicionado testes de status 200

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -2,9 +2,27 @@ import * as supertest from 'supertest'
 import app from './App'
 
 describe('App', () => {
-  it('works', () =>
+  it('welcome', () =>
     supertest(app)
       .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+  )
+  it('competitors', () =>
+  supertest(app)
+  .get('/competitors')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  )
+  it('competitions', () =>
+  supertest(app)
+  .get('/competitions')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  )
+  it('tournaments', () =>
+    supertest(app)
+      .get('/tournaments')
       .expect('Content-Type', /json/)
       .expect(200)
   )
